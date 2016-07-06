4407004e5969725dffd63b66d777fa196a3aaa95
Michael Mead <michaelmead007@icloud.com>
2016-07-06 14:39:27 -0500

    remove excess logs

--- app/facebook/facebook.js
+++ app/facebook/facebook.js
@@ -1,52 +1,20 @@
 angular.module('uplodr')
 
-// .controller('FacebookCtrl', function($scope, $http, facebookFactory) {
-//     facebookFactory.facebookAuth()
-// })
 
 .factory('facebookFactory', ($location) => {
-    // var facebookToken = null;
-    // var userID = null;
     return {
         facebookAuth: function() {
-            //var authWindow = new BrowserWindow({ width: 800, height: 600, show: false, 'node-integration': false });
-            // createWindow();
             var provider = new firebase.auth.FacebookAuthProvider();
             // var userID = firebase.auth().currentUser.uid
             if (firebase.auth().currentUser) {
                 $location.path('/todo');
                 // statement
-                // console.log("userID: ", userID);
             } else {
                 // statement
             return firebase.auth().signInWithPopup(provider)
             }
 
-            // .then(function(result) {
-            //     // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
-            //     // You can use these server side with your app's credentials to access the Twitter API.
-            //     facebookToken = result.credential.accessToken;
-            //     console.log("I am logged in!", result.user);
-            //     console.log("Get user ID", result.user.uid);
-            //     userID = result.user.uid;
-            //     var secret = result.credential.secret;
-            //     // The signed-in user info.
-            //     var user = result.user;
-            //     // ...
-            // }).catch(function(error) {
-            //     console.log("ARgh", error);
-            //     // Handle Errors here.
-            //     var errorCode = error.code;
-            //     var errorMessage = error.message;
-            //     // The email of the user's account used.
-            //     var email = error.email;
-            //     // The firebase.auth.AuthCredential type that was used.
-            //     var credential = error.credential;
-            //     // ...
-            // });
         },
-        // getUserID: function() {
-        //     return userID;
-        // }
+       
     }
 })
